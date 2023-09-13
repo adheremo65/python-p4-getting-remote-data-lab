a192306d1484f6189e3aa7ff13138d86570893c3
Abdela Umer <adheremo65@gmail.com>
2023-09-13 06:05:26 -0400

    api request lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -2,12 +2,18 @@ import requests
 import json
 
 class GetRequester:
+    import json
 
     def __init__(self, url):
         self.url = url
-
+        
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        json_str = response_body.decode('utf-8')
+        data = json.loads(json_str)
+        return data
+        
